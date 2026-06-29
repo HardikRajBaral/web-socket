3ba82d8c1422dcde012aecc967bb915e446ebc8e
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-06-29 12:36:41 +0545

    added heartbeat to check if client is still active

--- live-scoreboard/src/ws/server.js
+++ live-scoreboard/src/ws/server.js
@@ -7,7 +7,7 @@ function sendJson(socket,payload){
 
 function broadcast(wss,payload){
     for(const client of wss.clients){
-        if(client.readyState !== WebSocket.OPEN) return
+        if(client.readyState !== WebSocket.OPEN) continue
         client.send(JSON.stringify(payload))
     }
 }
@@ -19,10 +19,23 @@ export function attachWebSocketServer(server){
         maxPayload: 1024*1024
     })
     wss.on('connection',(socket )=>{
+        socket.isAlive = true
+        socket.on('pong',()=>{
+            socket.isAlive= true
+        })
         sendJson(socket,{type:'welcome'})
         socket.on('error',(err)=>{
             console.log(err)
         })
+        const interval= setInterval(()=>{
+        wss.clients.forEach((ws)=>{
+            if(ws.isAlive===false) return ws.terminate()
+            ws.isAlive= false
+            ws.ping()
+
+        })
+        },30000)
+        ws.on('close',()=>clearInterval(interval));
     })
     function broadCastMatchCreated(match){
         broadcast(wss,{type:'match_created',data:match})
