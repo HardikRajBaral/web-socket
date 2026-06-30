21d3ae8c677efed5cd669e77460b7db6f7cbdfff
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-06-30 15:06:11 +0545

    renaming the folder to sportz-backend

---
+++ Sportz-backend/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {matchRouter} from "./routes/matches.js";
+import  http from "node:http";
+import {attachWebSocketServer} from "./ws/server.js";
+import {securityMiddleware} from "./arcjet.js";
+import {commentaryRouter} from "./routes/commentary.js";
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0'
+
+const app = express();
+const server = http.createServer(app)
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Live scoreboard server is running." });
+});
+
+// app.use(securityMiddleware())
+
+app.use('/matches',matchRouter)
+app.use('/matches/:id/commentary',commentaryRouter)
+const {broadCastMatchCreated,broadcastCommentary}= attachWebSocketServer(server)
+
+app.locals.broadCastMatchCreated=broadCastMatchCreated
+app.locals.broadcastCommentary=broadcastCommentary
+
+server.listen(PORT,HOST, () => {
+  const baseUrl= HOST ==='0.0.0.0'?`http://localhost:${PORT}`: `http://${HOST}:${PORT}`
+  console.log(`Server running on : ${baseUrl}`);
+  console.log(`WebSocket  Server running on : ${baseUrl.replace('http','ws')}/ws`);
+
+});
